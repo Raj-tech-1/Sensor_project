b11c8f3fee464f5dbcd85611006e4974b6fdd1c6
Raj-tech-1 <rajsaket8987@gmail.com>
2025-09-05 10:27:54 +0530

    projec structure

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requiremnets]
+        requirements=[req.replace("\n", "") for req in requiremenets]
     return requirements
-
-if HYPEN_E_DOT in requirements:
-    requirements.remove(HYPEN_E_DOT)
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+        return requirements
 
 setup(
     name = 'Sensor Fault detection',
     version  = '0.0.1',
     author = 'Saket',
     author_mail ='rajsaket8987@gmail.com',
-    install_requirements=('requirements.txt'),
+    install_requires=('requirements.txt'),
     packages=find_packages(include=['sensor_fault_detection', 'sensor_fault_detection.*', 'test', 'test.*'])
 )
